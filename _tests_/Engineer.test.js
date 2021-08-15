cb2b23fd5bb23ab086ae9c105c30214bced8faf5
Dinimar Fish <tayla@tallisman.com.au>
2021-08-15 13:20:55 +1000

    added and passed Engineer.test.js

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,7 +1,25 @@
 const Engineer = require("../lib/Engineer");
 
+
 describe("Engineer Class", () => {
     //Then this is where all the BS is.
+    describe("employeeType and getRole", () => {
+    it("Correct employeeType = Engineer returns True", () => {
+        const Alice = new Engineer("Alice", "Engineer", "5748", "alice@email.com", "https://github.com/Dinimar-Fishboi")
+        expect((Alice.getRole()) === "Engineer").toBe(true);
+         })
+    it("Correct employeeType = Intern returns False", () => {
+        const Blair = new Engineer("Blair", "Intern", "9087", "blair@email.com", "https://github.com/Dinimar-Fishboi")
+        expect((Blair.getRole()) === "Engineer").toBe(false);
+        })
+      
+    })
 
+    describe("the Engineer should have a github account", ()=> {
+        it("There should be a githubURL for the Engineer Class", () => {
+            const Alice = new Engineer("Alice", "Engineer", "5748", "alice@email.com", "https://github.com/Dinimar-Fishboi")
+            expect((Alice.getGithub())).toBe("https://github.com/Dinimar-Fishboi");
+        })
+    })
 
 })
--- src/addEngineer.js
+++ src/addEngineer.js
@@ -10,7 +10,7 @@ function addEngineer(newEngineer) {
           <h4 class="card-title">${newEngineer.employeeName}</h4>
           <p class="card-text">Employee ID: ${newEngineer.employeeID}</p>
           <a href="mailto:${newEngineer.employeeEmail}" class="card-link">${newEngineer.employeeEmail}</a>
-          <a href="${newEngineer.githubUrl}" class="card-link">${newEngineer.githubUrl}</a>
+          <a href="${newEngineer.githubUrl}" class="card-link">GitHub</a>
         </div>
       </div>
     `
